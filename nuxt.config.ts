85a28d1fa8ddd507e1c4b8ef2a06bb5294ad2bdd
Cedric Lang-Roth <dnj855@yahoo.com>
2025-03-26 20:56:39 +0100

    Add google analytics support

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -66,5 +66,5 @@ export default defineNuxtConfig({
     '~/plugins/gsap.client.ts'
   ],
 
-  modules: ['@nuxt/image', '@nuxtjs/seo', 'nuxt-security']
+  modules: ['@nuxt/image', '@nuxtjs/seo', 'nuxt-security', 'nuxt-gtag']
 })
